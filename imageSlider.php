76b49432cdf0f38c4b5a76f9a768af19e667181c
Max <maximilian@rettinger-kahl.de>
2017-10-11 09:33:04 +0200

    Resized Buttons for smaller screens

--- imageSlider.php
+++ imageSlider.php
@@ -2,7 +2,7 @@
 	<button class="imageSliderButton" id="imageSlideLeftButton" onclick="prev()"><h1><</h1></button>
 	<ul class="imageSliderUl">
 		<?php
-		$pictureDir = "Images/";
+		$pictureDir = "./WebImageSlider/Images/";
 		$pictureFilenames = preg_grep('~\.(jpeg|jpg|png|JPG)$~', scandir($pictureDir));
 		foreach ($pictureFilenames as $pictureFilename) {
 			echo '<li><img src="'.$pictureDir.$pictureFilename.'" class="sliderImg"/></li>';
